ea668d98f3f9cbdda3303a9c6bc2f86ea4619a9f
Michael Nilsson <micnil487@gmail.com>
2018-09-28 21:46:42 +0200

    Simple grid layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import { createGlobalStyle } from 'styled-components'
 import Layout from '../components/layout'
-
+import { Flex, Box } from '../components/grid'
 
 const GlobalStyle = createGlobalStyle`
   html {
@@ -15,9 +15,16 @@ const GlobalStyle = createGlobalStyle`
 const IndexPage = () => (
   <Fragment>
     <Layout>
-      <h1>Hello World</h1>
+      <Flex>
+        <Box xs="12" sm="6" md="6" lg="4">
+          <h1>Hello World! this is awesome</h1>
+        </Box>
+        <Box xs="12" sm="6" md="6" lg="8">
+          <h1>Hello World! this is not awesome</h1>
+        </Box>
+      </Flex>
     </Layout>
-    <GlobalStyle/>
+    <GlobalStyle />
   </Fragment>
 )
 
